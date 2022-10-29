a2416883459b8d3cf30d389b03c39059c84ed02f
Vladimir Redkin <vsredkin01@gmail.com>
2022-10-29 09:06:47 +0300

    use own pow

--- rsa/rsa_alg.py
+++ rsa/rsa_alg.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import Iterator, List
 from primes import get_random_prime, get_random
 from euclid import egcd
 import math
@@ -48,6 +48,22 @@ def generate_keypair(primes_size: int) -> KeypairGenerationResult:
     pubKey = PublicKey(n, e)
     keypair = Keypair(pubKey, privKey)
     return KeypairGenerationResult(keypair, p, q, totient)
+
+
+def bits(n: int) -> Iterator[int]:
+    while n:
+        lsb = n % 2
+        yield lsb
+        n //= 2
+
+
+def pow(a: int, b: int, m: int):
+    s, c = 1, a
+    for bit in bits(b):
+        if bit:
+            s = s * c % m
+        c = c**2 % m
+    return s
 
 
 def encrypt(data: int, key: PublicKey) -> int:
