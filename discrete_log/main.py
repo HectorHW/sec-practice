38ce51116f9951a8e078696667472b415ac46e56
Vladimir Redkin <vsredkin01@gmail.com>
2022-11-26 08:56:24 +0300

    better error handling

--- discrete_log/main.py
+++ discrete_log/main.py
@@ -22,7 +22,7 @@ result_field = sg.InputText(tooltip="x:", disabled=True, size=size)
 result_text = sg.Text("x:")
 
 layout = [
-    [y_text, y_field, sg.Text("="), g_text, g_field, sg.Text(
+    [y_text, y_field, sg.Text("≡"), g_text, g_field, sg.Text(
         " ^ x"), sg.Text("mod "), m_field],
     [compute],
     [result_text, result_field]
@@ -56,9 +56,19 @@ while True:
         except ValueError as e:
             sg.Popup(f"недопустимое значение в поле {e.args[0]}")
             continue
-        result = baby_giant(y, g, m)
-        if result is None:
+        if y >= m:
+            sg.Popup(f"y должен быть < m")
+            continue
+        if g >= m:
+            sg.Popup(f"g должен быть < m")
+            continue
+        try:
+            result = baby_giant(y, g, m)
+            if result is None:
+                sg.Popup("для данных параметров ответа нет")
+                continue
+            else:
+                result_field.update(value=result)
+        except ValueError:
             sg.Popup("для данных параметров ответа нет")
             continue
-        else:
-            result_field.update(value=result)
